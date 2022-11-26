a46a9c3aa56ee2e893c9a3caf7ec629107631216
johnaeris <barrotjohnaeris@gmail.com>
2022-11-26 21:48:43 +0800

    Add color palette

--- app/src/main/java/com/example/kwizapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/kwizapp/ui/theme/Color.kt
@@ -6,3 +6,12 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+//My Color Palette
+val UfoGreen = Color(0xFF31E981)
+val DarkSlateGray = Color(0xFF35605A)
+val Onyx = Color(0xFF343E3D)
+val TuscanRed = Color(0xFF754043)
+val IndianRed = Color(0xFFDB5461)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
