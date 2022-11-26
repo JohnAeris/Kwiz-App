27b46e2f0cbaab47b0d1e187c5d3826a9bfff3b5
johnaeris <barrotjohnaeris@gmail.com>
2022-11-26 21:48:56 +0800

    Add typography

--- app/src/main/java/com/example/kwizapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/kwizapp/ui/theme/Type.kt
@@ -2,17 +2,55 @@ package com.example.kwizapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.kwizapp.R
 
 // Set of Material typography styles to start with
+
+val amaticBold = FontFamily(Font(R.font.amatic_sc_bold))
+val amaticRegular = FontFamily(Font(R.font.amatic_sc_regular))
+val indieFlower = FontFamily(Font(R.font.indie_flower_regular))
+
 val Typography = Typography(
+
+    h1 = TextStyle(
+        fontFamily = indieFlower,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+
+    h2 = TextStyle(
+        fontFamily = amaticBold,
+        fontSize = 20.sp
+    ),
+
+    h3 = TextStyle(
+        fontFamily = amaticRegular,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = amaticRegular,
         fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+
+    caption = TextStyle(
+        fontFamily = amaticRegular,
+        fontWeight = FontWeight.Thin,
+        fontSize = 12.sp
+    ),
+
+    button = TextStyle(
+        fontFamily = amaticBold,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp
     )
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
