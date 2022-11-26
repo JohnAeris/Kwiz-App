20e838cc219421e84846d619e5666d316c3e8b02
johnaeris <barrotjohnaeris@gmail.com>
2022-11-26 21:49:12 +0800

    Add color theme

--- app/src/main/java/com/example/kwizapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kwizapp/ui/theme/Theme.kt
@@ -7,15 +7,19 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = White,
+    primaryVariant = Black,
+    secondary = UfoGreen,
+    secondaryVariant = IndianRed,
+    background = Onyx
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Black,
+    primaryVariant = White,
+    secondary = DarkSlateGray,
+    secondaryVariant = TuscanRed,
+    background = White
 
     /* Other default colors to override
     background = Color.White,
